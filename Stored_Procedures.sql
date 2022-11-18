b1253638000ac1ecb32cdf3c3a41587b6fa9ad3e
BrainlessPOMO <74938211+BrainlessPOMO@users.noreply.github.com>
2022-11-18 13:13:05 +0200

    many deletes

--- Stored_Procedures.sql
+++ Stored_Procedures.sql
@@ -1,20 +1,19 @@
---
--- 3.1 procedure
---
--- CALL Procedure1('m', 4, '2002-10-05', '2011-05-09');
--- CALL Procedure1('s', 2, '2002-10-05', '2011-05-09');
+USE tvondemand2;
 
-DROP PROCEDURE IF EXISTS Procedure1;
+
 DELIMITER $$
-CREATE PROCEDURE Procedure1
+--
+-- Procedures
+--
+DROP PROCEDURE IF EXISTS `Procedure1`$$
+CREATE PROCEDURE `Procedure1` 
 (
-	IN charactiras char(1),
-	IN arithmos INT,
-	IN begin_date DATE,
-	IN end_date DATE
-)
+	IN `charactiras` CHAR(1), 
+	IN `arithmos` INT, 
+	IN `begin_date` DATE, 
+	IN `end_date` DATE
+)  
 BEGIN
-
 	IF (charactiras LIKE 'm') 
 		THEN
 			SELECT COUNT(*) AS Number,
@@ -39,29 +38,22 @@ BEGIN
   	   			LIMIT 0, arithmos;
   		END IF;
 END$$
-DELIMITER ;
---
--- 3.2 procedure
---
--- call Procedure2('s','APRIL.BURNS@sakilacustomer.org', '2005-06-20');
 
-DROP PROCEDURE IF EXISTS Procedure2;
-DELIMITER $$
-CREATE PROCEDURE Procedure2
+-- ---------------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS `Procedure2`$$
+CREATE PROCEDURE `Procedure2` 
 (
-	IN email VARCHAR(50), 
-	IN Hmerominia DATE
-)
+	IN `email` VARCHAR(50),
+	IN `Hmerominia` DATE
+)  
 BEGIN
 	SELECT COUNT (rental_id) AS Rentals
 	FROM rental
 	INNER JOIN user ON rental.user_id = user.user_id
 	WHERE user.email = email
-	and DATE(rental.rental_date) LIKE Hmerominia;
+	and DATE(rental.rental_date) LIKE 'Hmerominia';
 END$$
-DELIMITER ;
---
--- 3.3 procedure
 --
 DROP PROCEDURE IF EXISTS Procedure3;
 delimiter $$
@@ -72,7 +64,7 @@ begin
     declare movie_count_both int;
     declare movie_count_movies int;
     declare episodes_count_both int;
-	declare episodes_count_series int;
+		declare episodes_count_series int;
     declare earnings float;
     set counter = 0;
     
@@ -96,7 +88,7 @@ begin
 		);    
             
 		-- series_count_both = to noumero twn atomwn pou exoun enikiasei seires, kai plhrwnoun 0.1 € ana epeisodio seiras
-        set episodes_count_both = (
+			set episodes_count_both = (
 			SELECT count(`episode_id`) from `episode`
 			inner join `season` on `episode`.`season_id` = `season`.`season_id`
 			inner join `series` on `season`.`series_id` = `series`.`series_id`
@@ -109,12 +101,12 @@ begin
 			)
 		);
             
-        -- series_count_series = to noumero twn atomwn pou exoun enikiasei seires, kai plhrwnoun 0.2 € ana epeisodio seiras  
-        set episodes_count_series = (
-        SELECT count(`episode_id`) from `episode`
-			inner join `season` on `episode`.`season_id` = `season`.`season_id`
-			inner join `series` on `season`.`series_id` = `series`.`series_id`
-			WHERE series.series_id IN (
+		-- series_count_series = to noumero twn atomwn pou exoun enikiasei seires, kai plhrwnoun 0.2 € ana epeisodio seiras  
+		set episodes_count_series = (
+		SELECT count(`episode_id`) from `episode`
+		inner join `season` on `episode`.`season_id` = `season`.`season_id`
+		inner join `series` on `season`.`series_id` = `series`.`series_id`
+		WHERE series.series_id IN (
 				SELECT series.series_id AS number_of_series FROM series
 				INNER JOIN inventory ON series.series_id = inventory.series_id
 				INNER JOIN rental ON inventory.inventory_id = rental.inventory_id
@@ -126,84 +118,83 @@ begin
 		set earnings = movie_count_both * 0.3 + movie_count_movies * 0.4 + episodes_count_both * 0.1 + episodes_count_series * 0.2;
         
         -- emfanizw ta apotelesmata se mhnes
-        IF (counter = 0) THEN
+        IF counter = 0 THEN
 			select earnings as January_Earnings;
-		ELSE IF (counter = 1) THEN
+		ELSEIF counter = 1 THEN
 			select earnings as February_Earnings;
-		ELSE IF (counter = 2) THEN
+		ELSEIF counter = 2 THEN
 			select earnings as March_Earnings;
-		ELSE IF (counter = 3) THEN
+		ELSEIF counter = 3 THEN
 			select earnings as April_Earnings;
-		ELSE IF (counter = 4) THEN
+		ELSEIF counter = 4 THEN
 			select earnings as May_Earnings;
-		ELSE IF (counter = 5) THEN
+		ELSEIF counter = 5 THEN
 			select earnings as June_Earnings;
-		ELSE IF (counter = 6) THEN
+		ELSEIF counter = 6 THEN
 			select earnings as July_Earnings;
-		ELSE IF (counter = 7) THEN
+		ELSEIF counter = 7 THEN
 			select earnings as August_Earnings;
-		ELSE IF (counter = 8) THEN
+		ELSEIF counter = 8 THEN
 			select earnings as September_Earnings;
-		ELSE IF (counter = 9) THEN
+		ELSEIF counter = 9 THEN
 			select earnings as October_Earnings;
-		ELSE IF (counter = 10) THEN
+		ELSEIF counter = 10 THEN
 			select earnings as November_Earnings;
-		ELSE IF (counter = 11) THEN
+		ELSEIF counter = 11 THEN
 			select earnings as December_Earnings;
-        END IF;
-        set counter = counter +1;
-    end while;
-    
+		END IF;
+		set counter = counter + 1;
+  end while; 
+
 end$$
--- 3.4.a procedure
---
-DROP PROCEDURE IF EXISTS Procedure4;
-DELIMITER $$
-CREATE PROCEDURE Procedure4
-(	
-	IN first_last_name VARCHAR(45),
-    IN end_last_name VARCHAR(45)
-)
+
+-- -----------------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS `Procedure4`$$
+CREATE PROCEDURE `Procedure4` 
+(
+	IN `first_last_name` VARCHAR(45), 
+	IN `end_last_name` VARCHAR(45)
+)  
 BEGIN
-    SELECT count(`actor_id`) as plhthos FROM actor 
+  SELECT count(`actor_id`) as plhthos FROM actor 
 	WHERE last_name between first_last_name and end_last_name;
     
-    SELECT `first_name`, `last_name` FROM actor 
+  SELECT `first_name`, `last_name` FROM actor 
 	WHERE last_name between concat(first_last_name, '%') and concat( end_last_name, '%')
 	ORDER BY last_name ASC;
 END$$
-DELIMITER ;
 
---call Procedure4('Aco', 'Alm');
-
---
--- for 3.4.b 
--- make an index first
---
-ALTER TABLE `actor` ADD INDEX actor_index_last_name(last_name);
-
---
--- 3.4.b procedure
---
-DROP PROCEDURE IF EXISTS Procedure5;
-DELIMITER $$
-CREATE PROCEDURE Procedure5
-(	
-	IN last_name VARCHAR(45)
-)
-BEGIN
+DROP PROCEDURE IF EXISTS `Procedure5`$$
+CREATE PROCEDURE `Procedure5` (IN `last_name` VARCHAR(45))  BEGIN
 
 	declare count_actors int;
     -- check if there are more than 1 results
-    set count_actors = (select count(*) from actor where actor.last_name = last_name);
+  set count_actors = (select count(*) from actor where actor.last_name = last_name);
 	IF (count_actors > 1) THEN
-		select count_actors as plhthos;
+	select count_actors as plhthos;
 	END IF;
 	select  `actor_id`, `first_name`, `last_name` from actor
 	where actor.last_name = last_name;
 END$$
+
+DROP PROCEDURE IF EXISTS `ProcedureEkptwsi`$$
+CREATE PROCEDURE `ProcedureEkptwsi` (IN `Username` VARCHAR(25), IN `Hmerominia` DATE)  BEGIN
+	SELECT COUNT (rental_id) AS Enoikiaseis
+	FROM rental 
+	INNER JOIN user ON rental.user_id=user.user_id
+	WHERE rental.rental_date=CURRENT_TIMESTAMP
+	GROUP BY user.email;
+IF Enoikiaseis>=3 THEN
+	SELECT amount FROM rental INNER JOIN payment
+	ON rental.rental_id=payment.rental_id;
+  UPDATE payment.amount SET payment.amount=payment.default_price/2;
+  ELSE 
+	SELECT updated_amount FROM rental INNER JOIN payment
+  ON rental.rental_id=payment.rental_id;
+  UPDATE payment.amount SET payment.amount=payment.default_price;
+END IF;
+
+END$$
+
 DELIMITER ;
--- 1 result
--- call Procedure5('GUINESS');
--- more than 1 results
--- call Procedure5('Livingston');
---
+++ setupDB.sql
@@ -0,0 +1,308 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 12, 2022 at 09:51 PM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tvondemand`
+--
+DROP SCHEMA IF EXISTS tvondemand2;
+CREATE SCHEMA tvondemand2;
+USE tvondemand2;
+
+--
+-- Table structure for table `country`
+--
+DROP TABLE IF EXISTS `country`;
+CREATE TABLE IF NOT EXISTS `country` (
+  `country_id` int  NOT NULL AUTO_INCREMENT,
+  `country` varchar(50) NOT NULL,
+  PRIMARY KEY (`country_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `city`
+--
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE IF NOT EXISTS `city` (
+  `city_id` int  NOT NULL AUTO_INCREMENT,
+  `city` varchar(50) NOT NULL,
+  `country_id` int  NOT NULL,
+  PRIMARY KEY (`city_id`),
+  FOREIGN KEY (`city_id`) REFERENCES `country`(`country_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=590 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `address`
+--
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE IF NOT EXISTS `address` (
+  `address_id` int  NOT NULL AUTO_INCREMENT,
+  `address` varchar(50) NOT NULL,
+  `district` varchar(20) DEFAULT NULL,
+  `city_id` int  NOT NULL,
+  `postal_code` varchar(10) DEFAULT NULL,
+  `phone` varchar(20) NOT NULL,
+  PRIMARY KEY (`address_id`),
+  FOREIGN KEY (`city_id`) REFERENCES `city`(`city_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=603 DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Table structure for table `user`
+--
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int  NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `address_id` int  NOT NULL,
+  `active` int NOT NULL DEFAULT '1',
+  `create_date` datetime NOT NULL,
+  `role` set('customer','admin','employee') DEFAULT 'customer',
+  `type_of_reg` enum('series','movies','both') DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `fk_customer_address` (`address_id`),
+  FOREIGN KEY (`address_id`) REFERENCES `address`(`address_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=597 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE IF NOT EXISTS `log` (
+  `log_id` int  NOT NULL AUTO_INCREMENT,
+  `user_id` int  NOT NULL,
+  `modification_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_spec` enum('admin','employee','customer') CHARACTER SET utf8 DEFAULT 'customer',
+  `action` enum('insert','update','delete') CHARACTER SET utf8 NOT NULL,
+  `forTable` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`log_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=141 DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int  NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `language`
+--
+
+DROP TABLE IF EXISTS `language`;
+CREATE TABLE IF NOT EXISTS `language` (
+  `language_id` int  NOT NULL AUTO_INCREMENT,
+  `name` char(20) NOT NULL,
+  PRIMARY KEY (`language_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `series`
+--
+DROP TABLE IF EXISTS `series`;
+CREATE TABLE IF NOT EXISTS `series` (
+  `series_id` int  NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) NOT NULL,
+  `description` text,
+  `release_year` year(4) DEFAULT NULL,
+  `language_id` int  NOT NULL,
+  `rating` enum('G','PG','PG-13','R','NC-17') DEFAULT 'G',
+  `special_features` enum('Trailers','Commentaries','Deleted Scenes','Behind the Scenes') DEFAULT NULL,
+  `original_language_id` int  DEFAULT NULL,
+  PRIMARY KEY (`series_id`),
+  FOREIGN KEY (`language_id`) REFERENCES `language`(`language_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `season`
+--
+
+DROP TABLE IF EXISTS `season`;
+CREATE TABLE IF NOT EXISTS `season` (
+  `series_id` int  DEFAULT NULL,
+  `season_id` int  NOT NULL,
+  `season_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`season_id`),
+  FOREIGN KEY (`series_id`) REFERENCES `series`(`series_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `episode`
+--
+
+DROP TABLE IF EXISTS `episode`;
+CREATE TABLE IF NOT EXISTS `episode` (
+  `episode_id` int  NOT NULL AUTO_INCREMENT,
+  `season_id` int  NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `description` text,
+  `release_date` datetime DEFAULT NULL,
+  `length` varchar(255) DEFAULT NULL,
+  `rating` enum('G','PG','PG-13','R','NC-17') DEFAULT 'G',
+  PRIMARY KEY (`episode_id`),
+  FOREIGN KEY (`season_id`) REFERENCES `season`(`season_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=181 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `series_category`
+--
+
+DROP TABLE IF EXISTS `series_category`;
+CREATE TABLE IF NOT EXISTS `series_category` (
+  `series_id` int  NOT NULL,
+  `category_id` int  NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`series_id`,`category_id`),
+  FOREIGN KEY (`category_id`) REFERENCES `category`(`category_id`),
+  FOREIGN KEY (`series_id`) REFERENCES `series`(`series_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `movie`
+--
+DROP TABLE IF EXISTS `movie`;
+CREATE TABLE IF NOT EXISTS `movie` (
+  `movie_id` int  NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) NOT NULL,
+  `description` text,
+  `release_year` year(4) DEFAULT NULL,
+  `language_id` int  NOT NULL,
+  `length` int  DEFAULT NULL,
+  `rating` enum('G','PG','PG-13','R','NC-17') DEFAULT 'G',
+  `special_features` set('Trailers','Commentaries','Deleted Scenes','Behind the Scenes') DEFAULT NULL,
+  PRIMARY KEY (`movie_id`),
+  FOREIGN KEY (`language_id`) REFERENCES `language`(`language_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=975 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `actor`
+--
+DROP TABLE IF EXISTS `actor`;
+CREATE TABLE IF NOT EXISTS `actor` (
+  `actor_id` int  NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`actor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10026 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `movie_actor`
+--
+DROP TABLE IF EXISTS `movie_actor`;
+CREATE TABLE IF NOT EXISTS `movie_actor` (
+  `actor_id` int  NOT NULL,
+  `movie_id` int  NOT NULL,
+  PRIMARY KEY (`actor_id`,`movie_id`),
+  FOREIGN KEY (`movie_id`) REFERENCES `movie`(`movie_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `movie_category`
+--
+DROP TABLE IF EXISTS `movie_category`;
+CREATE TABLE IF NOT EXISTS `movie_category` (
+  `movie_id` int  NOT NULL,
+  `category_id` int  NOT NULL,
+  PRIMARY KEY (`movie_id`,`category_id`),
+  FOREIGN KEY (`category_id`) REFERENCES `category`(`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `inventory`
+--
+DROP TABLE IF EXISTS `inventory`;
+CREATE TABLE IF NOT EXISTS `inventory` (
+  `inventory_id` int  NOT NULL AUTO_INCREMENT,
+  `movie_id` int  DEFAULT NULL,
+  `series_id` int  DEFAULT NULL,
+  PRIMARY KEY (`inventory_id`),
+  FOREIGN KEY (`movie_id`) REFERENCES `movie`(`movie_id`),
+  FOREIGN KEY (`series_id`) REFERENCES `series`(`series_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4421 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `episode_actor`
+--
+DROP TABLE IF EXISTS `episode_actor`;
+CREATE TABLE IF NOT EXISTS `episode_actor` (
+  `episode_id` int  NOT NULL,
+  `actor_id` int  NOT NULL,
+  PRIMARY KEY (`episode_id`,`actor_id`),
+  FOREIGN KEY (`actor_id`) REFERENCES `actor`(`actor_id`),
+  FOREIGN KEY (`episode_id`) REFERENCES `episode`(`episode_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `rental`
+--
+DROP TABLE IF EXISTS `rental`;
+CREATE TABLE IF NOT EXISTS `rental` (
+  `rental_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rental_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `inventory_id` int  NOT NULL,
+  `user_id` int  NOT NULL,
+  PRIMARY KEY (`rental_id`),
+  UNIQUE KEY `rental_date` (`rental_date`,`inventory_id`,`user_id`),
+  FOREIGN KEY (`inventory_id`) REFERENCES `inventory`(`inventory_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16041 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `price_list`
+--
+DROP TABLE IF EXISTS `price_list`;
+CREATE TABLE IF NOT EXISTS `price_list` (
+  `price_list_id` int NOT NULL AUTO_INCREMENT,
+  `movie_prices` decimal(5,2) DEFAULT NULL,
+  `episode_prices` decimal(5,2) DEFAULT NULL,
+  `Both_prices` decimal(5,2) DEFAULT NULL,
+  `movie_id` int  NOT NULL,
+  `episode_id` int  NOT NULL,
+  `user_id` int  NOT NULL,
+  PRIMARY KEY (`price_list_id`),
+  FOREIGN KEY (`movie_id`) REFERENCES `movie`(`movie_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`),
+  FOREIGN KEY (`episode_id`) REFERENCES `episode`(`episode_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16041 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `payment`
+--
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE IF NOT EXISTS `payment` (
+  `payment_id` int  NOT NULL AUTO_INCREMENT,
+  `user_id` int  NOT NULL,
+  `rental_id` int(11) DEFAULT NULL,
+  `amount` decimal(5,2) NOT NULL,
+  `payment_date` datetime NOT NULL,
+  PRIMARY KEY (`payment_id`),
+  FOREIGN KEY (`rental_id`) REFERENCES `rental`(`rental_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15981 DEFAULT CHARSET=utf8mb4;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
